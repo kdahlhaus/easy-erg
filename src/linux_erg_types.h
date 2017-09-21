1f61e5afcbfe1a4dd5c934eb58074976cf24472a
Kevin Dahlhausen <kevin.dahlhausen@gmail.com>
2017-09-20 21:56:02 -0400

    linux compiles still needs c2api tied in

---
+++ src/linux_erg_types.h
@@ -0,0 +1,11 @@
+#ifndef linux_erg_types_h
+#define linux_erg_types_h
+
+#include <stdint.h>
+
+#define INT8_T short
+#define UINT16_T unsigned int
+#define UINT32_T unsigned long int
+#define ERRCODE_T short
+
+#endif
